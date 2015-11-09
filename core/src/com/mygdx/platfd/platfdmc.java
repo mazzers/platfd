0365f3cf10638ebf3ddf5c17ab4f343daddd4c24
mazzers <vetal.zt@gmail.com>
2015-11-09 23:54:37 +0100

    +init

--- core/src/com/mygdx/platfd/platfdmc.java
+++ core/src/com/mygdx/platfd/platfdmc.java
@@ -5,23 +5,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class platfdmc extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Table container;
+
 	@Override
 	public void create () {
+		container = new Table();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("lala.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0,1,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 17, 100);
 		batch.end();
 	}
 }
